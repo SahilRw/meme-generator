0b9bef731624d9c87acd6604eaa3fc97df97be12
Sahil R <sahilrw000@gmail.com>
2023-05-07 16:01:03 +0530

    styled the component

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -2,9 +2,19 @@ import React from 'react'
 
 const Meme = () => {
   return (
-    <div>
-      <h1>Content</h1>
-    </div>
+    <main className='p-10'>
+        <form className='grid grid-cols-2 grid-rows-2 md:grid-cols-1 md:grid-rows-2 gap-4'>
+            <input 
+                type="text"
+                placeholder='Top Text' 
+                className='rounded indent-1.5 border border-gray-300 p-2'/>
+            <input 
+                type="text"
+                placeholder='Bottom Text' 
+                className='rounded indent-1.5 border border-gray-300 p-2'/>
+            <button className='col-span-full cursor-pointer rounded bg-gradient-to-r from-purple-700 to-purple-900 text-white border-0'>Get a new meme image 🖼</button>
+        </form>    
+    </main>
   )
 }
 
