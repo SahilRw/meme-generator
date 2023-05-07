57477c788e3a266cff1f1c10cad85716ebd64c16
Sahil R <sahilrw000@gmail.com>
2023-05-07 14:40:05 +0530

    added header component working on meme component

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+      <header className='flex items-center h-16 bg-gradient-to-r from-purple-700 to-purple-900 text-white p-5'>
+        <img src="" alt="" className='h-full mr-1.5'/>
+        <h2 className='text-xl mr-auto'>Meme Generator</h2>
+      </header>
+  )
+}
+
+export default Header
---
+++ src/components/Meme.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Meme = () => {
+  return (
+    <div>
+      <h1>Content</h1>
+    </div>
+  )
+}
+
+export default Meme
